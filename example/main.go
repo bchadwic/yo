c9a370e0d5d39671203c6bf0bd6d2a65d7db1f8a
bchadwic <benchadwick87@gmail.com>
2021-08-25 23:08:29 -0700

    working on edit

--- example/main.go
+++ example/main.go
@@ -2,42 +2,47 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/bchadwic/yo/pkg/multi"
-	"github.com/bchadwic/yo/pkg/prompt"
-	"github.com/bchadwic/yo/yo"
+	"os/exec"
 )
 
 func main() {
-	yo := yo.ProdYo()
-	// fmt.Println()
-	// (&prompt.Prompt{
-	// 	Message: "Do you like icecream?",
-	// 	Choices: []string{"Yes", "No"},
+	// yo := yo.ProdYo()
+	// // fmt.Println()
+	// // (&prompt.Prompt{
+	// // 	Message: "Do you like icecream?",
+	// // 	Choices: []string{"Yes", "No"},
+	// // }).Prompt(yo)
+	// // fmt.Println()
+	// // (&prompt.Prompt{
+	// // 	Message: "How is life?",
+	// // 	Choices: []string{"Good", "Bad", "Great"},
+	// // 	Default: "Good",
+	// // }).Prompt(yo)
+	// // fmt.Println()
+	// input, err := (&prompt.Prompt{
+	// 	Choices:       []string{"Good", "Bad", "Great"},
+	// 	Attempts:      2,
+	// 	Default:       "Awesome",
+	// 	CaseSensitive: true,
 	// }).Prompt(yo)
-	// fmt.Println()
-	// (&prompt.Prompt{
-	// 	Message: "How is life?",
-	// 	Choices: []string{"Good", "Bad", "Great"},
-	// 	Default: "Good",
-	// }).Prompt(yo)
-	// fmt.Println()
-	input, err := (&prompt.Prompt{
-		Choices:       []string{"Good", "Bad", "Great"},
-		Attempts:      2,
-		Default:       "Awesome",
-		CaseSensitive: true,
-	}).Prompt(yo)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 
-	fmt.Println(input + "\n\n")
-	input2, err := (&multi.Multi{
-		Message: "Enter a value yo",
-	}).Multi(yo)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println("OUTPUT:\n(" + input2 + ")")
+	// fmt.Println(input + "\n\n")
+	// input2, err := (&query.Query{
+	// 	Message: "Enter a value yo",
+	// }).Query(yo)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println("OUTPUT:\n(" + input2 + ")")
+	cmd := exec.Command("which", "vim")
+	err := cmd.Run()
+	fmt.Println(err)
+	// cmd := exec.Command("vim", "test.txt")
+	// cmd.Stdin = os.Stdin
+	// cmd.Stdout = os.Stdout
+	// err := cmd.Run()
+	// fmt.Println(err)
 }
