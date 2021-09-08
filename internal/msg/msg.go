f0e9da29a9c9b18e036629e54bb382e20aa48106
Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
2021-09-08 11:02:14 -0700

    added reader to library (#6)

--- internal/msg/msg.go
+++ internal/msg/msg.go
@@ -6,6 +6,8 @@ const (
 	ReturnValue = "return twice to save and quit"
 
 	// error
-	InvalidValue  = "input is invalid"
-	InvalidChoice = "enter a valid choice"
+	InvalidValue    = "input is invalid"
+	InvalidChoice   = "enter a choice supplied"
+	InvalidPath     = "unable to find path %s"
+	InvalidAttempts = "invalid amount of attempts"
 )
