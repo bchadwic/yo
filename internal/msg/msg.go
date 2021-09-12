e75fd70c950d1e2f86adfa2ec35b4a06aa344f1b
Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
2021-09-12 11:08:45 -0700

    tested write (#15)
    * added tests to write
    
    * Completed tests for write

--- internal/msg/msg.go
+++ internal/msg/msg.go
@@ -6,8 +6,11 @@ const (
 	ReturnValue = "return twice to save and quit"
 
 	// error
-	InvalidChoice   = "enter a choice supplied"
-	InvalidValue    = "invalid input"
-	InvalidAttempts = "invalid amount of attempts"
-	InvalidPath     = "invalid path %s"
+	InvalidChoice     = "enter a choice supplied"
+	InvalidValue      = "invalid input"
+	InvalidAttempts   = "invalid amount of attempts"
+	InvalidPath       = "invalid path %s"
+	InvalidFile       = "invalid file name %s"
+	InvalidType       = "invalid modify type"
+	InvalidPermission = "invalid permission"
 )
--- pkg/file/write/write.go
+++ pkg/file/write/write.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/bchadwic/yo/internal/msg"
 	"github.com/bchadwic/yo/yo"
 )
 
@@ -16,7 +17,7 @@ type Write struct {
 }
 
 const (
-	REPLACE   = 0
+	REPLACE   = 1
 	APPEND    = os.O_APPEND
 	OVERWRITE = os.O_TRUNC
 	CREATE    = os.O_CREATE
@@ -34,22 +35,28 @@ func outputWrite(w *Write, y *yo.Yo) {
 }
 
 func inputWrite(w *Write, y *yo.Yo) error {
+	if w.File == "" {
+		return fmt.Errorf(msg.InvalidFile, w.File)
+	}
+	if w.Type == 0 {
+		return fmt.Errorf(msg.InvalidType)
+	}
 	_, err := os.Stat(w.File)
 	if w.Type != CREATE && err != nil {
-		return err
+		return fmt.Errorf(msg.InvalidType)
 	}
 	ty := w.Type
 	if w.Type == REPLACE {
 		ty = CREATE
 	}
 	permission := w.Permission
-	if permission == 0 {
+	if permission >= 0777 {
 		permission = 0644
 	}
 	var f *os.File
 	f, err = os.OpenFile(w.File, os.O_WRONLY|ty, permission)
 	if err != nil {
-		return err
+		return fmt.Errorf(msg.InvalidPermission)
 	}
 	_, err = f.Write([]byte(w.Text))
 	return err
