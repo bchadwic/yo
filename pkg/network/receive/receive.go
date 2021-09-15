6fe65da547accd0b18654e5cb2a71fd319f04d3f
bchadwic <benchadwick87@gmail.com>
2021-09-15 00:26:36 -0700

    saving for different dev computer

--- pkg/network/receive/receive.go
+++ pkg/network/receive/receive.go
@@ -1,1 +1,28 @@
 package receive
+
+import (
+	"fmt"
+
+	"github.com/bchadwic/yo/yo"
+)
+
+type Recieve struct {
+	Endpoint *Endpoint
+	Preface  string
+	Output   bool
+}
+
+func (r *Recieve) Recieve(y *yo.Yo) (string, error) {
+	outputRecieve(r, y)
+	return "", nil
+}
+
+func outputRecieve(r *Recieve, y *yo.Yo) {
+	if r.Preface != "" {
+		fmt.Fprintf(y.Out, "%s\n", r.Preface)
+	}
+}
+
+func inputRecieve() {
+
+}
